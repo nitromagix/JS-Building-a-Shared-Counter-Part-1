247354ffef62aa1c5e14e30e7a167babcc348263
Martin <nitromagix@gmail.com>
2022-04-21 20:19:07 -0700

    finished assignment

--- public/index.js
+++ public/index.js
@@ -1,7 +1,24 @@
-function main(){
+const HTTP = "https://";
+
+async function getData() {
+    let response = await fetch("http://127.0.0.1:9002/goobi");
+    let responseText =  await response.text();
+    return responseText;
+}
+
+let cr = console.log;
+
+// cr("test");
+// cr(getUrl("www.google.com"));
+
+const main = async () => {
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+
+    let text = await getData();
+    let jsonString = JSON.stringify(text);
+    cr(jsonString);
 
     let countValue = 0;
 
@@ -19,4 +36,9 @@ function main(){
     decrementButton.addEventListener('click', decrement);
     countContainer.textContent = countValue;
 }
+
+
+
 main()
+
+
--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const liveServer = require('live-server')
 const sleep = time => new Promise(resolve => setTimeout(resolve,time))
 const isWin = process.platform === 'win32'
 async function main(){
-    spawn( isWin ? 'npx.cmd' : 'npx', ['json-server', '--watch', '--port', '9001', 'db.json' ], { stdio: 'inherit'});
+    spawn( isWin ? 'npx.cmd' : 'npx', ['json-server', '--watch', '--port', '9002', 'db.json' ], { stdio: 'inherit'});
     await sleep(1000) // <-- Wait one second, so that the json-server has started by the time the live-server opens.
     liveServer.start({
-        port: 9000,
+        port: 9002,
         logLevel: 0,
         root: './public'
     })
